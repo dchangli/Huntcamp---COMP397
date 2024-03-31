a28386a3d18e78e4a6a1222abc7c4b8358a509b7
cmcwatta <74875036+cmcwatta@users.noreply.github.com>
2024-03-31 11:11:45 -0400

    Pooled Projectiles

---
+++ Huntcamp/Assets/Scripts/PooledProjectile/PoolingManager.cs
@@ -0,0 +1,4 @@
+
+
+//public class PoolingManager : GenericPoolManager<PooledProjectile>
+//{ }
---
+++ Huntcamp/Assets/Scripts/PooledProjectile/ProjectilePool.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ProjectilePool : MonoBehaviour
+{
+    [SerializeField] private float _lifeTime;
+    [SerializeField] private float _maxLifeTime = 3f;
+
+    // Why on Enable and not Start
+    // OnEnable is called when the object is activated. 
+    private void OnEnable()
+    {
+        _lifeTime = 0;
+    }
+
+    private void Update()
+    {
+        _lifeTime += Time.deltaTime;
+       
+    }
+}
